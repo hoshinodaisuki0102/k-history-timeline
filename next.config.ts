03f40f294d242e368df433e5715582b09c8f7186
츄베릎 <mychan0204@gmail.com>
2026-03-15 23:31:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production";
+const isGithubPages =
+  process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
